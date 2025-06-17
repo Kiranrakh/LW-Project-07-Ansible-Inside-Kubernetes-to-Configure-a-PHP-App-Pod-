ca1ea84f27b05f52d9ed9f1aa05c8b35d6b7ac81
Kiran Barikrao Rakh <124512771+Kiranrakh@users.noreply.github.com>
2025-06-17 15:49:12 +0530

    profile.php

---
+++ ansible/files/profile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Kiran’s DevOps App</title>
+  <style>
+    body { background: #111; color: white; text-align: center; font-family: Ari>
+    .card { background: #222; border-radius: 10px; padding: 30px; display: inli>
+    h1 { color: #00f0ff; }
+  </style>
+</head>
+<body>
+  <div class="card">
+    <h1>🚀 Kiran Rakh</h1>
+    <p>DevOps Engineer in the Making</p>
+    <p>LinuxWorld | AWS | Docker | Kubernetes | Ansible</p>
+  </div>
+</body>
+</html>
